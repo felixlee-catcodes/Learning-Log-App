ac33715e6d6a1e322ea5fa48dd4a87d3fc8e9f03
C. Felix Lee <felixlee.music@gmail.com>
2024-07-11 23:09:19 -0400

    new_topic.html created; user can now add new topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path('topics/<int:topic_id>/', views.topic, name='topic'),
 
     # Page for adding a new topic:
-    path('new_topic/', views.new_topic, name='new_topics')
+    path('new_topic/', views.new_topic, name='new_topic')
 ]
